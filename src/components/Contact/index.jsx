d44979cabde2e619bfcc829c4b626f3bd3c65708
herisetyoaji <heriaji72@gmail.com>
2021-07-01 18:34:47 +0700

    web portofolio

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,27 @@
+import "./style.css";
+
+const About = () => {
+  return (
+    <div className="contact">
+      <h1>Contact me!</h1>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3967.2380299244483!2d106.158993352517!3d-6.098610090973369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sid!2sid!4v1625032542737!5m2!1sid!2sid"
+        frameBorder="0"
+        allowFullScreen=""
+        aria-hidden="false"
+        tabIndex="0"
+      ></iframe>
+
+      <h4>Phone</h4>
+      <p>0812-9051-4753</p>
+
+      <h4>Email</h4>
+      <p>heriaji72@gmail.com</p>
+
+      <h4>Address</h4>
+      <p>Griya Lopang Indah FG 36 No. 12A, RT/RT : 02/14, Kel. Unyur, Kec. Serang, Kota Serang, Banten</p>
+    </div>
+  );
+};
+
+export default About;
